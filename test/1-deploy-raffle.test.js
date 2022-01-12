4cdd6eb6e4077cee26cc87682e59b8f1d7eb4794
Rafael Zago <rafaelszago@gmail.com>
2022-01-12 12:48:47 -0300

    refactor: test deploy raffle contract

--- test/1-deploy-raffle.test.js
+++ test/1-deploy-raffle.test.js
@@ -1,5 +1,4 @@
-const { makeSut, mockCreateRaffleParams, makeRaffle } = require('./helpers/mock-raffle')
-const truffleAssert = require('truffle-assertions')
+const { makeSut } = require('./helpers/mock-raffle')
 
 contract('RaffleContract', async (accounts) => {
   describe('Test raffle contract', async () => {
@@ -7,20 +6,6 @@ contract('RaffleContract', async (accounts) => {
       const contract = await makeSut()
       const admin = await contract.admin()
       assert.equal(admin, accounts[0])
-    })
-
-    it('should return an execption if try claim reward and raffle is active', async () => {
-      const contract = await makeSut()
-      const raffleParams = mockCreateRaffleParams()
-      const { raffleId } = await makeRaffle(contract, accounts[0], raffleParams)
-
-      await truffleAssert.fails(
-        contract.claimReward(raffleId, {
-          from: accounts[0]
-        }),
-        'revert',
-        'Raffle is running'
-      )
     })
   })
 })
