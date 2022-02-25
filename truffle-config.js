f78aca8a8c2d26de5aa51042cdf1513543020667
Rafael Zago <rafaelszago@gmail.com>
2022-02-25 15:09:49 -0300

    chore: add bsc deployment settings

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,24 @@
+/* eslint-disable camelcase */
+require('dotenv').config()
+
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+
 module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
       port: 8545,
-      // eslint-disable-next-line camelcase
       network_id: '*' // Match any network id
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(
+        process.env.BSC_SECRET,
+        'https://bsc-dataseed1.binance.org'
+      ),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
     }
   },
   compilers: {
@@ -12,5 +26,12 @@ module.exports = {
       version: '^0.8.10'
     }
   },
-  plugins: ['solidity-coverage']
+
+  api_keys: {
+    bscscan: process.env.BSCSCAN_API_KEY
+  },
+  plugins: [
+    'solidity-coverage',
+    'truffle-plugin-verify'
+  ]
 }
